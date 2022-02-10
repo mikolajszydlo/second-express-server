14f1b490fef2f57f5c8b33331072c2b7f4976969
Mikolaj Szydlo <mikolaj.szydlo@gmail.com>
2022-02-10 22:12:39 +0100

    Add handelbars

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `./views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', hbs.engine());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/user', (req, res) => {
-  res.show('forbidden.html');
+  res.render('forbidden');
 })
 
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' });
 });
 
 app.use((req, res) => {
-  res.show('404.html');
+  res.render('404');
 });
 
 app.listen(8000, () => {
